63222bc28ce8d0717f857a38afd23a6eb7562f8c
Penis Lord <37201831+Trickst6r@users.noreply.github.com>
2019-12-21 14:55:28 +0700

    Add files via upload

---
+++ Assignment/Ass11.sql
@@ -0,0 +1,288 @@
+﻿
+-- 1. Create a database named Ass11
+CREATE DATABASE dbAss11
+GO 
+
+USE dbAss11
+GO 
+
+-- 2. Create the following tables in the database, applying the specified integrity constraints
+CREATE TABLE tbItem
+( 
+	ItemCode VARCHAR(5) PRIMARY KEY NONCLUSTERED,
+	ItemDesc VARCHAR(30) NOT NULL,
+	Qoh INT NOT NULL ,					--Quantity on hand
+	ReOrdLvl INT ,						--Re-order level
+	Price INT							--Price per unit
+)
+GO 
+
+CREATE TABLE tbIssueMaster
+(
+	IssueCode VARCHAR(5) PRIMARY KEY,
+	IssueDate DATE NOT NULL,
+	DeptCode VARCHAR(5) NOT NULL
+)
+GO 
+
+CREATE TABLE tbIssueDetails
+(
+	IssueCode VARCHAR(5) FOREIGN KEY REFERENCES dbo.tbIssueMaster(IssueCode) ,
+	ItemCode VARCHAR(5) FOREIGN KEY REFERENCES dbo.tbItem(ItemCode),
+	IssueQty INT	--Quantity issued
+	PRIMARY KEY (IssueCode,ItemCode)
+)
+GO 
+
+-- 3. Insert the following data into the above tables:
+INSERT dbo.tbItem
+    (
+        ItemCode,
+        ItemDesc,
+        Qoh,
+        ReOrdLvl,
+        Price
+    )
+VALUES
+    (
+        'IT001', -- ItemCode - varchar(5)
+        'Refrigerator', -- ItemDesc - varchar(30)
+        50,  -- Qoh - int
+        400,  -- ReOrdLvl - int
+        4000   -- Price - int
+    ),
+    (
+        'IT002', -- ItemCode - varchar(5)
+        'Television', -- ItemDesc - varchar(30)
+        100,  -- Qoh - int
+        75,  -- ReOrdLvl - int
+        3000   -- Price - int
+    ),
+    (
+        'IT003', -- ItemCode - varchar(5)
+        'Washing Machine', -- ItemDesc - varchar(30)
+        250,  -- Qoh - int
+        250,  -- ReOrdLvl - int
+        2000   -- Price - int
+    ),
+    (
+        'IT004', -- ItemCode - varchar(5)
+        'Microware', -- ItemDesc - varchar(30)
+        200,  -- Qoh - int
+        150,  -- ReOrdLvl - int
+        3500   -- Price - int
+    )
+GO 
+
+INSERT dbo.tbIssueMaster
+    (
+        IssueCode,
+        IssueDate,
+        DeptCode
+    )
+VALUES
+    (
+        'IS001',        -- IssueCode - varchar(5)
+        '20030112', -- IssueDate - date
+        'D0001'         -- DeptCode - varchar(5)
+    ),
+    (
+        'IS002',        -- IssueCode - varchar(5)
+        '20030218', -- IssueDate - date
+        'D0003'         -- DeptCode - varchar(5)
+    ),
+    (
+        'IS003',        -- IssueCode - varchar(5)
+        '20030218', -- IssueDate - date
+        'D0008'         -- DeptCode - varchar(5)
+    ),
+    (
+        'IS004',        -- IssueCode - varchar(5)
+        '20030815', -- IssueDate - date
+        'D0002'         -- DeptCode - varchar(5)
+    )
+GO 
+
+INSERT dbo.tbIssueDetails
+    (
+        IssueCode,
+        ItemCode,
+        IssueQty
+    )
+VALUES
+    (
+        'IS001', -- IssueCode - varchar(5)
+        'IT001', -- ItemCode - varchar(5)
+        15   -- IssueQty - int
+    ),
+    (
+        'IS002', -- IssueCode - varchar(5)
+        'IT003', -- ItemCode - varchar(5)
+        5   -- IssueQty - int
+    ),
+    (
+        'IS003', -- IssueCode - varchar(5)
+        'IT004', -- ItemCode - varchar(5)
+        2   -- IssueQty - int
+    ),
+    (
+        'IS004', -- IssueCode - varchar(5)
+        'IT002', -- ItemCode - varchar(5)
+        1   -- IssueQty - int
+    ),
+    (
+        'IS004', -- IssueCode - varchar(5)
+        'IT004', -- ItemCode - varchar(5)
+        5   -- IssueQty - int
+    )
+GO 
+
+-- 4. Create clustered index ixDesc on column ItemDesc of table tbItem, by descending order.
+
+CREATE CLUSTERED INDEX ixDesc ON dbo.tbItem(ItemDesc DESC)
+GO 
+
+/*
+	5. Perform the following queries on the above table
+		a. Display the details of Item table
+		b. Display a stock report showing item code, description, quantity on hand, re-order level and price giving user friendly names to the columns.
+		c. Display the details of items having a price less than or equal to 2000
+		d. Write a query to display an item issue report showing issue code, issue date, department code, item description and quantity issued
+*/
+
+--5a. Display the details of Item table
+
+SELECT * FROM dbo.tbItem
+GO 
+
+--5b. Display a stock report showing item code, description, quantity on hand, re-order level and price giving user		friendly names to the columns.
+
+SELECT ItemCode AS [Item Code],ItemDesc AS [Description], Qoh AS [Quantity on hand],ReOrdLvl AS [Re-Order Level],Price
+FROM dbo.tbItem 
+GO  
+
+--5c. Display the details of items having a price less than or equal to 2000
+
+SELECT ItemCode AS [Item Code],ItemDesc AS [Description], Qoh AS [Quantity on hand],ReOrdLvl AS [Re-Order Level],Price
+FROM dbo.tbItem
+WHERE Price<=2000
+GO 
+
+--5d. Write a query to display an item issue report showing issue code, issue date, department code, item description	and quantity issued
+
+SELECT a.IssueCode,a.IssueDate,a.DeptCode,c.ItemDesc,b.IssueQty
+FROM (dbo.tbIssueMaster a JOIN dbo.tbIssueDetails b ON b.IssueCode = a.IssueCode) JOIN dbo.tbItem c ON b.ItemCode=c.ItemCode
+GO 
+
+/*
+	6. Create a view vwQoH display a list of items where the quantity on hand is equal to the re-order level
+*/
+CREATE VIEW vwQoH
+AS
+SELECT *
+FROM dbo.tbItem 
+WHERE Qoh=ReOrdLvl
+GO 
+
+SELECT * FROM dbo.vwQoH
+GO 
+
+/*
+	7. Create a store procedure named uspIncreaseQOH to increase the quantity on hand of a given item code.
+	Hint: The procedure accept two parameters: itemcode and amount to be added to QOH column in Item table
+*/
+
+CREATE PROC uspIncreaseQOH
+@ma_item VARCHAR(5),@soluong INT
+AS
+BEGIN
+	--Bước 1 : Hiện bảng theo yêu cầu
+	SELECT * 
+	FROM dbo.tbItem
+	WHERE ItemCode=@ma_item ;
+	--Bước 2 : Update dữ liệu
+	UPDATE dbo.tbItem SET Qoh+=@soluong WHERE ItemCode=@ma_item ;
+	--Bước 3 : Hiện bảng theo yêu cầu	
+	SELECT * 
+	FROM dbo.tbItem
+	WHERE ItemCode=@ma_item ; 	 
+END
+GO 
+
+--Thử store với Item IT001 thêm 10Qoh
+EXEC dbo.uspIncreaseQOH
+    @ma_item = 'IT001', -- varchar(5)
+    @soluong = 10   -- int
+GO 
+
+/*
+	8. Create a trigger for tgIssueDetails table which will perform the following task when a new record is inserted:
+		a. Ensure that the quantity issued is less than or equal to quantity on hand
+		b. Update the Qoh column of Item table by subtracting the quantity issued
+*/
+
+
+CREATE TRIGGER tgIssueDetails
+ON dbo.tbIssueDetails
+FOR INSERT AS 
+BEGIN 
+	DECLARE @soluong_issue INT , @soluong_item INT  
+	SELECT @soluong_issue = a.IssueQty FROM Inserted a ;
+	SELECT @soluong_item = b.Qoh FROM dbo.tbItem b;
+	-- 8a.Ensure that the quantity issued is less than or equal to quantity on hand
+	IF @soluong_issue > @soluong_item 
+	BEGIN
+	    PRINT N'Số lượng hàng bị lỗi phải ít hơn hoặc bằng số lượng hàng đang có'
+		ROLLBACK
+	END	
+	-- 8b. Update the Qoh column of Item table by subtracting the quantity issued
+	--(Cập nhật là cột Qoh của bảng Item bằng cách trừ đi số lượng bị lỗi)
+	UPDATE dbo.tbItem 
+	SET Qoh -= @soluong_issue
+	FROM dbo.tbItem a JOIN Inserted b ON b.ItemCode = a.ItemCode	
+END 
+GO 
+
+--Test Trigger kiểm tra Insert IT004 có IssueQty = 500 có thông báo lỗi không !!
+SELECT * FROM dbo.tbIssueDetails
+SELECT * FROM dbo.tbItem
+GO 
+
+INSERT dbo.tbIssueDetails
+    (
+        IssueCode,
+        ItemCode,
+        IssueQty
+    )
+VALUES
+    (
+        'IS001', -- IssueCode - varchar(5)
+        'IT004', -- ItemCode - varchar(5)
+        500   -- IssueQty - int
+    )
+GO 
+
+--Test Trigger kiểm tra Insert IT004 có IssueQty = 10 thì bên bảng Item có update Qoh không ?
+INSERT dbo.tbIssueDetails
+    (
+        IssueCode,
+        ItemCode,
+        IssueQty
+    )
+VALUES
+    (
+        'IS001', -- IssueCode - varchar(5)
+        'IT004', -- ItemCode - varchar(5)
+        10   -- IssueQty - int
+    )
+GO 
+
+
+
+SELECT * FROM dbo.tbIssueDetails
+SELECT * FROM dbo.tbItem
+GO 
+
+
+
+
---
+++ Assignment/Pretest.sql
@@ -0,0 +1,359 @@
+﻿--Pretest
+
+/*
+	1. Create a database named dbPretest
+*/
+CREATE DATABASE dbPretest
+GO 
+
+USE dbPretest
+GO 
+
+/*
+	2. Create the following tables in Database dbPretest
+*/
+CREATE TABLE tbRoom
+(
+	RoomNo INT PRIMARY KEY NONCLUSTERED,
+	[Type] VARCHAR(20) CHECK([Type] IN ('VIP','Double','Single')),
+	UnitPrice MONEY CHECK(UnitPrice BETWEEN 0 AND 999) 
+)
+GO 
+
+CREATE TABLE tbBooking
+(
+	BookingNo INT ,
+	RoomNo INT FOREIGN KEY REFERENCES dbo.tbRoom(RoomNo),
+	TouristName VARCHAR(20) NOT NULL,
+	DateFrom DATETIME,
+	DateTo DATETIME ,
+	PRIMARY KEY NONCLUSTERED(BookingNo,RoomNo) 
+)
+GO 
+
+ALTER TABLE dbo.tbBooking
+	ADD CONSTRAINT DateTo CHECK(DateTo>DateFrom)
+GO 
+
+INSERT dbo.tbRoom
+    (
+        RoomNo,
+        [Type],
+        UnitPrice
+    )
+VALUES
+    (
+        101,   -- RoomNo - int
+        'Single',  -- Type - varchar(20)
+        100 -- UnitPrice - money
+    ),
+    (
+        102,   -- RoomNo - int
+        'Single',  -- Type - varchar(20)
+        100 -- UnitPrice - money
+    ),
+    (
+        103,   -- RoomNo - int
+        'Double',  -- Type - varchar(20)
+        250 -- UnitPrice - money
+    ),
+    (
+        201,   -- RoomNo - int
+        'Double',  -- Type - varchar(20)
+        250 -- UnitPrice - money
+    ),
+    (
+        202,   -- RoomNo - int
+        'Double',  -- Type - varchar(20)
+        300 -- UnitPrice - money
+    ),
+    (
+        203,   -- RoomNo - int
+        'Single',  -- Type - varchar(20)
+        150 -- UnitPrice - money
+    ),
+    (
+        301,   -- RoomNo - int
+        'VIP',  -- Type - varchar(20)
+        900 -- UnitPrice - money
+    )
+GO 
+
+INSERT dbo.tbBooking
+    (
+        BookingNo,
+        RoomNo,
+        TouristName,
+        DateFrom,
+        DateTo
+    )
+VALUES
+    (
+        1,         -- BookingNo - int
+        101,         -- RoomNo - int
+        'Julia',        -- TouristName - varchar(20)
+        '20141112', -- DateFrom - datetime
+        '20141114'  -- DateTo - datetime
+    ),
+    (
+        1,         -- BookingNo - int
+        103,         -- RoomNo - int
+        'Julia',        -- TouristName - varchar(20)
+        '20141212', -- DateFrom - datetime
+        '20141213'  -- DateTo - datetime
+    ),
+    (
+        2,         -- BookingNo - int
+        301,         -- RoomNo - int
+        'Bill',        -- TouristName - varchar(20)
+        '20150110', -- DateFrom - datetime
+        '20150114'  -- DateTo - datetime
+    ),
+    (
+        3,         -- BookingNo - int
+        201,         -- RoomNo - int
+        'Ana',        -- TouristName - varchar(20)
+        '20150112', -- DateFrom - datetime
+        '20150114'  -- DateTo - datetime
+    ),
+    (
+        3,         -- BookingNo - int
+        202,         -- RoomNo - int
+        'Ana',        -- TouristName - varchar(20)
+        '20150112', -- DateFrom - datetime
+        '20150114'  -- DateTo - datetime
+    ),
+	(
+        3,         -- BookingNo - int
+        301,         -- RoomNo - int
+        'Seohyun',        -- TouristName - varchar(20)
+        '20181210', -- DateFrom - datetime
+        '20181212'  -- DateTo - datetime
+    )
+GO 
+
+/*
+	3. Create a clustered index ixName on column TouristName of table tbBooking
+	4. Create a non-clustered index ixType on column Type of table tbRoom
+*/
+
+CREATE CLUSTERED INDEX ixName ON dbo.tbBooking(TouristName)
+GO
+CREATE NONCLUSTERED INDEX ixType ON dbo.tbRoom([Type])
+GO 
+
+/*
+	5. Create a view vwBooking to see the information about bookings in year 2014 which contain the following columns:
+	BookingNo, TouristName, RoomNo,Type, UnitPrice, DateFrom, DateTo. The definition of view must be encrypted.
+*/
+
+CREATE VIEW vwBooking
+WITH ENCRYPTION
+AS 
+SELECT a.BookingNo,a.TouristName,b.RoomNo,[b].[Type],b.UnitPrice,a.DateFrom,a.DateTo
+FROM dbo.tbBooking a JOIN dbo.tbRoom b ON b.RoomNo = a.RoomNo
+WHERE YEAR(a.DateFrom)=2014
+GO 
+
+SELECT * FROM dbo.vwBooking
+GO 
+
+/*
+	6. Create a stored procedure name uspPriceDecrease will down the unit price of double rooms a given amount (input parameter). If non value given, display a list of rooms, sorted by price .
+*/
+
+CREATE PROC uspPriceDecrease
+@gia MONEY 
+AS 
+BEGIN	
+	--Lệnh 1 
+	SELECT * 
+	FROM dbo.tbRoom
+	WHERE [Type]='Double'
+	ORDER BY UnitPrice ;
+	--Lệnh 2
+	UPDATE dbo.tbRoom SET UnitPrice-=@gia WHERE [Type]='Double'
+	--Lệnh 3
+	IF @@ROWCOUNT=0
+	BEGIN
+		SELECT *
+		FROM dbo.tbRoom
+		ORDER BY UnitPrice DESC		
+	END
+	ELSE 
+	BEGIN 
+		SELECT *
+		FROM dbo.tbRoom
+		WHERE [Type]='Double'
+		ORDER BY UnitPrice DESC 
+	END 
+END  
+GO 
+
+SELECT * FROM dbo.tbRoom
+GO 
+
+EXEC dbo.uspPriceDecrease
+    @gia = 10 -- money
+GO 
+/*
+	7. Create a stored procedure name uspSpecificPriceIncrease will increment the unit price of a given room (input parameter) by a given amount (input parameter) and return the number of rooms (output parameter) which have room rate above 250.
+*/
+
+CREATE PROC uspSpecificPriceIncrease
+@maphong INT,@gia MONEY,@sophong INT OUTPUT 
+AS
+BEGIN
+	--Lệnh 1
+	SELECT *
+	FROM dbo.tbRoom
+	WHERE RoomNo=@maphong	;
+	--Lệnh 2
+	UPDATE dbo.tbRoom SET UnitPrice += @gia WHERE RoomNo=@maphong
+	--Lệnh 3
+	SET @sophong = (SELECT COUNT(*) FROM dbo.tbRoom WHERE UnitPrice>=250) ;
+	--Lệnh 4 
+	SELECT *
+	FROM dbo.tbRoom
+	ORDER BY UnitPrice ;
+END 
+GO 
+
+SELECT * FROM dbo.tbRoom
+GO 
+
+DECLARE @sophong INT;
+EXEC dbo.uspSpecificPriceIncrease
+    @maphong = 103,              -- int
+    @gia = 20,               -- money
+    @sophong = @sophong OUTPUT -- int
+SELECT @sophong AS N'Tổng số phòng có giá >250' 
+GO 
+
+/*
+	8. Create a trigger named tgBookingRoom that allows one booking order having 3 rooms maximum.
+*/
+alter TRIGGER tgBookingRoom
+ON dbo.tbBooking
+AFTER INSERT,UPDATE AS
+BEGIN
+	DECLARE @sophong INT
+	DECLARE @maso INT
+	SELECT @maso=Inserted.BookingNo FROM Inserted
+	SELECT @sophong=COUNT(*) FROM dbo.tbBooking WHERE BookingNo=@maso GROUP BY BookingNo
+	IF @sophong>3
+	BEGIN
+		PRINT N'Mỗi Booking Order chi duoc dang ky toi da 3 phong'
+		ROLLBACK
+    END 
+END
+GO 
+
+
+
+sp_helptext tgBookingRoom
+
+SELECT * FROM dbo.tbBooking
+SELECT COUNT(BookingNo) FROM dbo.tbBooking 
+SELECT COUNT(RoomNo)+1 FROM dbo.tbBooking GROUP BY BookingNo
+--Test trigger
+INSERT dbo.tbBooking
+    (
+        BookingNo,
+        RoomNo,
+        TouristName,
+        DateFrom,
+        DateTo
+    )
+VALUES
+    (
+        3,         -- BookingNo - int
+        103,         -- RoomNo - int
+        'Taeyeon',        -- TouristName - varchar(20)
+        '20181210', -- DateFrom - datetime
+        '20181212'  -- DateTo - datetime
+    ),
+	(
+        3,         -- BookingNo - int
+        101,         -- RoomNo - int
+        'Taeyeon',        -- TouristName - varchar(20)
+        '20181210', -- DateFrom - datetime
+        '20181212'  -- DateTo - datetime
+    )
+
+GO 
+
+
+SELECT * FROM dbo.tbBooking
+GO 
+
+
+/*
+	9. Create a trigger named tgRoomUpdate that doing the following (using try-catch structure) : 
+		If new price is equal to 0 and this room has not existed in tbBooking, then remove it from tbRoom table
+		Else display an error message and roll back transaction.
+*/
+--???
+
+
+alter TRIGGER tgRoomUpdate
+ON dbo.tbRoom 
+INSTEAD OF UPDATE AS
+BEGIN
+	DECLARE @sophong INT
+	DECLARE @dongia money 
+	SELECT @sophong=Inserted.RoomNo,@dongia=Inserted.UnitPrice FROM Inserted
+	BEGIN TRY 
+		IF @dongia=0 
+		BEGIN 
+			DELETE FROM dbo.tbRoom WHERE RoomNo=@sophong
+		END 
+		ELSE 
+		BEGIN
+			UPDATE dbo.tbRoom SET UnitPrice=@dongia WHERE RoomNo=@sophong
+		END 
+	END TRY 
+	BEGIN CATCH
+		SELECT ERROR_LINE()  N'Dòng lỗi',ERROR_MESSAGE() N'Thanh báo lỗi' , ERROR_NUMBER() N'Mã số lỗi sai'
+		
+		ROLLBACK
+	END CATCH
+END
+GO 
+
+
+
+SELECT * FROM dbo.tbRoom
+SELECT * FROM dbo.tbBooking
+GO 
+--Test Trigger
+--Tạo thêm 1 room không có trong booking để cập nhật giá =0
+INSERT dbo.tbRoom
+    (
+        RoomNo,
+        Type,
+        UnitPrice
+    )
+VALUES
+    (
+        404,   -- RoomNo - int
+        'VIP',  -- Type - varchar(20)
+        500 -- UnitPrice - money
+    )
+GO 
+
+
+--Check xem có Delete room 404 khi Price =0 không ?
+UPDATE dbo.tbRoom SET UnitPrice=0 WHERE RoomNo=404
+GO 
+
+SELECT * FROM dbo.tbRoom
+
+----Check xem có báo lỗi khi Update room 301 Price =0 không ?
+UPDATE dbo.tbRoom SET UnitPrice=0 WHERE RoomNo=301
+GO 
+
+--Check khi update giá bình thường
+UPDATE dbo.tbRoom SET UnitPrice=999 WHERE RoomNo=301
+GO 
+
